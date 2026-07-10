b821e4364577916fde3f95ad59d357ddbd508127
kaif0910 <mdkaifperwez@gmail.com>
2026-07-10 18:14:11 +0530

    added the generate-report tool

--- tools/report.py
+++ tools/report.py
@@ -2,6 +2,7 @@ from langchain.tools import tool
 from services.report_service import report_service
 @tool
 def generate_report(report_content: str):
+    """Generate a PDF report."""
     return report_service.generate_report(
         report_content
     )
