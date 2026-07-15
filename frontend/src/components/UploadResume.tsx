ec54d85b07c33f85c5920c0e6d01067338e05186
kaif0910 <mdkaifperwez@gmail.com>
2026-07-15 12:27:36 +0530

    created uploadresume.tsx

---
+++ frontend/src/components/UploadResume.tsx
@@ -0,0 +1,29 @@
+export default function UploadResume(){
+
+    return(
+
+        <div
+            className="rounded-lg border
+            border-dashed
+            bg-white
+            p-8
+            text-center"
+        >
+
+            <h2 className="font-semibold">
+
+                Upload Resume
+
+            </h2>
+
+            <p className="mt-2 text-gray-500">
+
+                Resume upload coming soon.
+
+            </p>
+
+        </div>
+
+    );
+
+}
