ce0f5b902f43f088dc85abcee77e59eb80411f64
kaif0910 <mdkaifperwez@gmail.com>
2026-07-16 17:30:24 +0530

    updated the client api and removed the hardcoded content type

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,9 +2,9 @@ import axios from "axios"
 
 const api = axios.create({                        //create a configured HTTP client
     baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+    // headers: {
+    //     "Content-Type": "application/json",   # multipart request fails because content-type was hardcoded because of this file uploads fails 
+    // },
 });
 
 export default api;
