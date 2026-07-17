9b770f8b273b6b71b603ba4354ebf1e8a22a9729
kaif0910 <mdkaifperwez@gmail.com>
2026-07-17 15:05:38 +0530

    updated the conversation_service with all the required methods

--- services/conversation_service.py
+++ services/conversation_service.py
@@ -1,23 +1,115 @@
 import json 
 from pathlib import Path
+from datetime import datetime
+from api.models.conversation import (
+    Conversation,
+    create_conversation
+)
 
-DATA_FILE = Path("data/conversations.json")
+from api.models.message import ChatMessage
 
 class ConversationService:
 
-    def load(self):
-        if not DATA_FILE.exists():
-            DATA_FILE.write_text("[]")
+    def __init__(self):
+        self.file = Path("data/conversations.json")
 
-        with open(DATA_FILE, "r") as f:
+        if not self.file.exists():
+            self.file.parent.mkdir(exist_ok=True)
+            self.file.write_text("[]")
+
+    def load(self):
+
+        with open(self.file, "r") as f:
             return json.load(f)
 
     def save(self, conversations):
-        with open(DATA_FILE, "w") as f:
+        with open(self.file, "w") as f:
             json.dump(
                 conversations,
                 f,
                 indent=4
             )
+
+    def list_conversations(self):
+        return self.load()
+
+    def create_conversation(self):
+
+        conversations =  self.load()
+
+        Conversation = create_conversation()
+
+        conversations.append(
+            Conversation.model_dump(
+                mode="json"
+            )
+        )
+
+        self.save(conversations)
+
+        return Conversation
+
+    def get_conversation(
+            self,
+            conversation_id: str
+    ):
+        conversations = self.load()
+
+        for conversation in conversations:
+            if conversation["id"] == conversation_id:
+                return conversation 
+        return None
+
+    def save_messages(
+            self,
+            conversation_id: str ,
+            messages: list[ChatMessage]
+    ):
+        conversations = self.load()
+
+        for conversation in conversations :
+            if conversation["id"] == conversation_id:
+
+                conversation["messages"] = [
+                    message.model_dump(
+                        model= "json"
+                    )
+
+                    for message in messages
+                ]
+
+                conversation["updated_at"] = (
+                    datetime.now().isoformat
+                )
+
+                break
+            self.save(conversations)
+
+
+
+    def update_title(self, conversation_id: str , title: str):
+        conversations = self.load()
+        for conversation in conversations:
+            if conversation["id"] == conversation_id:
+                conversation["title"] = title
+
+                conversation["updated_at"] = (
+                    datetime.now().isoformat()
+                )
+
+                break
+        self.save(conversations)
+
+    def delete_conversation(self, conversation_id: str):
+
+        conversations = self.load()
+
+        conversations = [
+            conversations
+            for conversation in conversations: 
+            if Conversation["id"] != conversation_id
+        ]
+        self.save(conversations)
+
 
 Conversation_service = ConversationService()
