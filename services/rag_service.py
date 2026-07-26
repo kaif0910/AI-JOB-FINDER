b74c65ffbbcf1706ce4de59801e28a82fffbe7e9
kaif0910 <mdkaifperwez@gmail.com>
2026-07-26 19:20:24 +0530

    removed RAG and simply parsed the resume to the llm beacause it was taking a lot of memory

--- services/rag_service.py
+++ services/rag_service.py
@@ -11,69 +11,81 @@ load_dotenv()
 class RAGService:
 
     def __init__(self):
-        self.embedding_model = None
+        # self.embedding_model = None
 
-        self.vector_store = None
-        self.retriever = None
+        # self.vector_store = None
+        # self.retriever = None
+        self.resume_text = ""
 
-    def initialize(self):
-        if self.embedding_model is not None:
-            return 
-        print("Loading Embedding Model...")
-        self.embedding_model = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2"
-        )
-        print("Embedding Model Loaded.")
-    def load_resume(self, pdf_path: str, collection_name= "resume") -> None:
+    # def initialize(self):
+    #     if self.embedding_model is not None:
+    #         return 
+    #     print("Loading Embedding Model...")
+    #     self.embedding_model = HuggingFaceEmbeddings(
+    #         model_name="sentence-transformers/all-MiniLM-L6-v2"
+    #     )
+    #     print("Embedding Model Loaded.")
+    def load_resume(self, pdf_path: str) -> None:
 
-        self.initialize()
+        # self.initialize()
         # Load PDF
         loader = PyPDFLoader(pdf_path)
         documents = loader.load()
 
-
-        # Split into chunks
-        splitter = RecursiveCharacterTextSplitter(
-            chunk_size=800,
-            chunk_overlap=150
+        self.resume_text = "\n\n".join(
+            doc.page_content
+            for doc in documents
         )
 
-        chunks = splitter.split_documents(documents)
+
+
+        # Split into chunks
+        # splitter = RecursiveCharacterTextSplitter(
+        #     chunk_size=800,
+        #     chunk_overlap=150
+        # )
+
+        # chunks = splitter.split_documents(documents)
 
 
         # Create vector database
-        self.vector_store = Chroma(
-            collection_name=collection_name,
-            embedding_function=self.embedding_model,
-            persist_directory="./chroma_db"
-        )
+        # self.vector_store = Chroma(
+        #     collection_name=collection_name,
+        #     embedding_function=self.embedding_model,
+        #     persist_directory="./chroma_db"
+        # )
 
-        self.vector_store.reset_collection()
+        # self.vector_store.reset_collection()
         # Store chunks
-        self.vector_store.add_documents(chunks)
+        # self.vector_store.add_documents(chunks)
 
         # Create retriever
-        self.retriever = self.vector_store.as_retriever(
-            search_type= "mmr",
-            search_kwargs={
-                "k": 3,
-                "fetch_k": 15
-            }
-        )
+        # self.retriever = self.vector_store.as_retriever(
+        #     search_type= "mmr",
+        #     search_kwargs={
+        #         "k": 3,
+        #         "fetch_k": 15
+        #     }
+        # )
 
     def search(self, question: str) -> str:
 
-        if self.retriever is None:
+        # if self.retriever is None:
+        #     raise Exception("Resume has not been loaded.")
+
+        # docs = self.retriever.invoke(question)
+
+        # context = "\n\n".join(
+        #     doc.page_content
+        #     for doc in docs
+        # )
+
+        # return context
+
+        if not self.resume_text:
             raise Exception("Resume has not been loaded.")
 
-        docs = self.retriever.invoke(question)
-
-        context = "\n\n".join(
-            doc.page_content
-            for doc in docs
-        )
-
-        return context
+        return self.resume_text
 
 
 rag_service = RAGService()   #singleton instance
