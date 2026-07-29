c9f077340da57b9d4786066a68f2a76ca6765163
kaif0910 <mdkaifperwez@gmail.com>
2026-07-29 15:58:42 +0530

    updated the conversation router

--- api/routers/conversation.py
+++ api/routers/conversation.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi import Query
 
 from services.conversation_service import conversation_service
 
@@ -8,27 +9,31 @@ router = APIRouter(
 )
 
 @router.get("")
-def list_conversations():
-    return conversation_service.list_conversations()
+def list_conversations(session_id: str = Query(...)):
+    return conversation_service.list_conversations(session_id=session_id)
 
 @router.post("")
-def create_conversation():
-    return conversation_service.create_conversation()
+def create_conversation(session_id: str = Query(...)):
+    return conversation_service.create_conversation(session_id=session_id)
 
 @router.get("/{conversation_id}")
 def get_conversation(
-    conversation_id: str
+    conversation_id: str,
+    session_id: str = Query(...)
 ):
     return conversation_service.get_conversation(
-        conversation_id
+        conversation_id,
+        session_id
     )
 
 @router.delete("/{conversation_id}")
 def delete_conversation(
-    conversation_id: str
+    conversation_id: str,
+    session_id: str = Query(...)
 ):
     conversation_service.delete_conversation(
-        conversation_id
+        conversation_id,
+        session_id
     )
 
     return {
@@ -39,10 +44,12 @@ def delete_conversation(
 @router.patch("/{conversation_id}/title")
 def update_title(
     conversation_id: str,
-    title: str
+    session_id: str = Query(...),
+    title: str = Query(...)
 ):
     conversation_service.update_title(
         conversation_id,
+        session_id,
         title
     )
 
